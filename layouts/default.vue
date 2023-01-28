b74066931413a1e49bdf3ef763d81269182e9241
Digby Knight <digby@digby.net>
2023-01-28 08:27:27 +0000

    Header changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="flex flex-col min-h-full w-full px-2 md:px-0 font-sans font-400 bg-white dark:bg-gradient-to-br dark:from-slate-800 dark:to-slate-800 text-stone-700 dark:text-stone-300"
+    class="flex flex-col w-full mxa lg:max-w-80rem md:px-0 font-sans font-400 bg-white dark:bg-gradient-to-br dark:from-slate-800 dark:to-slate-800 text-stone-700 dark:text-stone-300"
   >
     <header
       class="container w-screen-xl m-auto py-2 md:py-4 md:sticky md:top-0 z-10 bg-white dark:bg-slate-800/70 backdrop-filter backdrop-blur-lg border-b border-gray-400"

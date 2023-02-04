f37f640dc5c962a1f96d1a19ddc7385be4d8e71a
Digby Knight <digby@digby.net>
2023-02-04 19:27:50 +0000

    Adding PWA again

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,6 +63,25 @@ export default defineNuxtConfig({
       ],
     },
   },
+  pwa: {
+    scope: "/",
+    srcDir: "./service-worker",
+    filename: "sw.ts",
+    strategies: "injectManifest",
+    injectRegister: false,
+    includeManifestIcons: false,
+    manifest: false,
+    injectManifest: {
+      globPatterns: [
+        "**/*.{js,json,css,html,txt,svg,png,ico,webp,woff,woff2,ttf,eot,otf,wasm}",
+      ],
+      globIgnores: ["emojis/**", "shiki/**", "manifest**.webmanifest"],
+    },
+    devOptions: {
+      enabled: process.env.VITE_DEV_PWA === "true",
+      type: "module",
+    },
+  },
   routeRules: {
     // Static page generated on-demand, revalidates in background
     "/blog/**": { swr: true },

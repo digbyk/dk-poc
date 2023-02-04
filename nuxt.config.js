dc3b4b3c29a19be629819f12066ab51cd126cb9f
Digby Knight <digby@digby.net>
2023-02-04 21:03:34 +0000

    Adding dk logos

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,17 +71,17 @@ export default defineNuxtConfig({
       theme_color: "#ffffff",
       icons: [
         {
-          src: "pwa-192x192.png",
+          src: "dk-logo-192x192.png",
           sizes: "192x192",
           type: "image/png",
         },
         {
-          src: "pwa-512x512.png",
+          src: "dk-logo-512x512.png",
           sizes: "512x512",
           type: "image/png",
         },
         {
-          src: "pwa-512x512.png",
+          src: "dk-logo-512x512.png",
           sizes: "512x512",
           type: "image/png",
           purpose: "any maskable",

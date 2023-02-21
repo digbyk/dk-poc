b0a9829ca8f3625a24cd84433c81386e10aa95d3
Digby <digby@digby.net>
2023-02-21 12:45:59 +0000

    Changes

--- pages/spa.vue
+++ pages/spa.vue
@@ -1,6 +1,12 @@
+<script lang="ts" setup>
+const date = () => {
+  return new Date()
+}
+</script>
+
 <template>
   <div>
     <h1>SPA</h1>
-    <h2>{{ new Date() }}</h2>
+    <h2>{{ date }}</h2>
   </div>
 </template>
